4cb56de8ca4d61040d7393074d8b380ad7855fbb
Denys Periel <denys@periel.biz>
2020-04-05 11:29:29 +0200

    Revrite data fro yesterday with more precise set

--- parse.py
+++ parse.py
@@ -2,15 +2,17 @@ import requests
 import pandas as pd
 import os
 import os.path
-from time import time
-from datetime import date
+from datetime import date, timedelta
 
 def main():
     if not os.path.exists("./stats"):
         os.mkdir("./stats")
 
-    iso_date = date.fromtimestamp(time()).isoformat()
-    path_with_date = f'./stats/{iso_date}.csv'
+    iso_date = date.today().isoformat()
+    yesterday = date.today() - timedelta(days=1)
+    iso_date_yesterday = yesterday.isoformat()
+    path_with_date = to_path(iso_date)
+    path_with_date_yesterday = to_path(iso_date_yesterday)
 
     if os.path.exists(path_with_date):
         print('You already have the data for today')
@@ -22,18 +24,26 @@ def main():
     print(f'Data received with status code {stats_page.status_code}')
     if stats_page.status_code == 200:
         all_tables = pd.read_html(stats_page.content)
+        print(len(all_tables))
         data_frame = all_tables[0]
+        date_frame_yesterday = all_tables[1]
+        remove_if_exists(path_with_date_yesterday)
 
-        path_to_current = f'./stats/current.csv'
-        removeIfExists(path_to_current)
+        path_to_current = to_path('current')
+        remove_if_exists(path_to_current)
 
         print('Saving files:')
         data_frame.to_csv(path_to_current, encoding="utf-8", index=False)
         print('\t' + path_to_current)
         data_frame.to_csv(path_with_date, encoding="utf-8", index=False)
         print('\t' + path_with_date)
+        date_frame_yesterday.to_csv(path_with_date_yesterday, encoding="utf-8", index=False)
+        print('\t' + path_with_date_yesterday)
 
-def removeIfExists(path):
+def to_path(name):
+    return f'./stats/{name}.csv'
+
+def remove_if_exists(path):
     if os.path.exists(path):
         os.remove(path)
 
